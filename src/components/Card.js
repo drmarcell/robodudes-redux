bf4056be775b9c39a6a0f382cf5aaf0fe3df7846
drmarcell <92795736+drmarcell@users.noreply.github.com>
2022-01-09 09:09:02 +0100

    added to GH

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => {
+    //const {name, email, id} = props;
+    return (
+        <div className='tc bg-washed-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+}
+
+export default Card;
